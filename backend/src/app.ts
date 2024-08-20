8560fab54bb3433e2f142b8b21a757413becb40f
Jack Krause <jmkrause@iastate.edu>
2024-08-20 12:40:54 -0500

    setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,30 @@
 import { Request, Response } from 'express'
-
+import { Error } from 'mongoose'
 const express = require('express')
 const app = express()
+const cors = require("cors")
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
 	res.send("hello world!")
 })
 
+app.use(cors())
+app.use(bodyParser.json())
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/vuefitness')
+	.then(() => {
+		console.info("MongoDB connected")
+	})
+	.catch((error: Error) => {
+		console.error("error connecting to DB", error)
+	})
+
+
 app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
+	console.log(`Server is running on http://localhost:${port}`)
 })
